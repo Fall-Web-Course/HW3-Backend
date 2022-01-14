f3453578fc7c15d3687d7e9cc9a3e0f8f7e6ba6d
mhbahmani <mhbahmani79@gmail.com>
2022-01-14 17:58:48 +0330

    minor changes on project structure

--- main.go
+++ main.go
@@ -1,31 +1,11 @@
 package main
 
 import (
-	users "github.com/Fall-Web-Course/HW3/users"
-	notes "github.com/Fall-Web-Course/HW3/notes"
-
-	"github.com/gin-gonic/gin"
-
-	"fmt"
+	db "github.com/Fall-Web-Course/HW3/db"
+	router "github.com/Fall-Web-Course/HW3/router"
 )
 
-func init_router(LISTEN_ADDRESS string) {
-	router := gin.Default();
-
-	router.POST("/notes/new", notes.NewNote);
-	router.GET("/notes", notes.GetNote); // /note_id
-	router.PUT("/notes", notes.UpdateNote); // /note_id
-	router.DELETE("/notes", notes.DeleteNote); // /note_id
-	
-	router.POST("/users/register", users.Register);	
-	router.POST("/users/login", users.Login);	
-
-	router.Run(LISTEN_ADDRESS); // Listens on 0.0.0.0:8080 by default
-}
-
 func main() {
-	PORT := Getenv("PORT", "8080");	HOST_LAN_IP := Getenv("LAN_HOST_IP", "127.0.0.1");
-	LISTEN_ADDRESS := fmt.Sprintf("%s:%s", HOST_LAN_IP, PORT);
-
-	init_router(LISTEN_ADDRESS)
+	db.InitDb();
+	router.InitRouter();
 }
