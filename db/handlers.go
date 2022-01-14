3883e038b0fbc9bda42744c74919bb729efbfdfa
mhbahmani <mhbahmani79@gmail.com>
2022-01-14 17:57:52 +0330

    fix db dsn

--- db/handlers.go
+++ db/handlers.go
@@ -8,9 +8,9 @@ import (
 	notes "github.com/Fall-Web-Course/HW3/notes"
 )
 
-var DB_DSN string = "host=localhost user=hw3 password=webhw3password dbname=hw3 port=5433 sslmode=disable TimeZone=Asia/Tehran"
+var DB_DSN string = "host=localhost user=web password=webhw3password dbname=hw3 port=5433 sslmode=disable TimeZone=Asia/Tehran"
 var db, err = gorm.Open(postgres.Open(DB_DSN), &gorm.Config{})
 
-func init_db() {
+func InitDb() {
 	db.AutoMigrate(&users.User{}, &notes.Note{})
 }
