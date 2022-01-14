58cf8bb4061574ccc3a5f9d639b21487074297bf
mhbahmani <mhbahmani79@gmail.com>
2022-01-14 21:36:10 +0330

    update Note model

--- notes/models.go
+++ notes/models.go
@@ -2,16 +2,28 @@ package notes
 
 import (
 	"github.com/Fall-Web-Course/HW3/db"
+	"github.com/Fall-Web-Course/HW3/users"
 	"gorm.io/gorm"
 )
 
 type Note struct {
 	gorm.Model
-	AuthorRefer 	int
-	Author			string	`gorm:"foreignKey:AuthorRefer"`
-	Text			string
+	UserRefer	int
+	User		users.User	`gorm:"foreignKey:UserRefer"`
+	Text		string
+}
+
+type NewNoteInput struct {
+	AuthorId	int		`json:"user_id"`
+	Text		string	`json:"text"`
 }
 
 func InsertToDb(note Note) {
 	db.GetDb().Create(&note)
+}
+
+func GetUserByid(user_id int) users.User {
+	var user users.User
+	db.GetDb().Find(&user, user_id)
+	return user
 }
