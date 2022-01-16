7c49651f12965b2589819da867d12526a8c5fb80
mhbahmani <mhbahmani79@gmail.com>
2022-01-16 18:31:47 +0330

    add set and get functions to cache package

--- cache/handlers.go
+++ cache/handlers.go
@@ -1,6 +1,10 @@
 package cache
 
 import (
+	"context"
+	"strconv"
+	"time"
+
 	init_cache "github.com/Fall-Web-Course/HW3/cache/init"
 	proto "github.com/Fall-Web-Course/HW3/proto"
 )
@@ -10,4 +14,20 @@ var client = init_cache.ConnectToCache("localhost:50051")
 
 func GetCacheClient() proto.CacherClient {
 	return client
+}
+
+func SetKey(key string, value string) (*proto.KeyPair, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	int_key, _ := strconv.ParseInt(key, 10, 64)
+	r, err := client.SetKey(ctx, &proto.KeyPair{Key: int_key, Value: value})
+	return r, err
+}
+
+func GetKey(key string) (*proto.Value, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	int_key, _ := strconv.ParseInt(key, 10, 64)
+	r, err := client.GetKey(ctx, &proto.Key{Key: int_key})
+	return r, err
 }
