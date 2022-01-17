cee996051ed77f7d2c9cb726702c70be00b68067
mhbahmani <mhbahmani79@gmail.com>
2022-01-17 15:43:03 +0330

    implement delete node endpoint

--- notes/handlers.go
+++ notes/handlers.go
@@ -2,11 +2,12 @@ package notes
 
 import (
 	"net/http"
-	
+
 	"github.com/gin-gonic/gin"
 
 	cache "github.com/Fall-Web-Course/HW3/cache"
 	users "github.com/Fall-Web-Course/HW3/users"
+	db "github.com/Fall-Web-Course/HW3/db"
 )
 
 
@@ -32,7 +33,7 @@ func GetNote(c *gin.Context) {
 	note_id := c.Param("note_id")
 	value, err := cache.GetKey(note_id)
 	if err != nil {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"Message": "Something went wrong",
 		})
 		return
@@ -46,7 +47,7 @@ func GetNote(c *gin.Context) {
 		go cache.SetKey(note_id, text)
 	}
 
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"Text": text,
 	})
 }
@@ -65,7 +66,16 @@ func UpdateNote(c *gin.Context) {
 }
 
 func DeleteNote(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "delete_note",
-	})
+	note_id := c.Param("note_id")
+
+	out := db.GetDb().Delete(&Note{}, note_id)
+	if out.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Something bad happend",
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "delete_note",
+		})
+	}
 }
