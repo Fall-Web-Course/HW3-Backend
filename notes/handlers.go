a87bc47458cc6109489dbde9247055d4c8226020
mhbahmani <mhbahmani79@gmail.com>
2022-01-17 17:07:47 +0330

    add checking user access

--- notes/handlers.go
+++ notes/handlers.go
@@ -1,6 +1,8 @@
 package notes
 
 import (
+	"fmt"
+	"strings"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,27 +12,37 @@ import (
 	db "github.com/Fall-Web-Course/HW3/db"
 )
 
-
 func NewNote(c *gin.Context) {
 	var new_note NewNoteInput
 	c.BindJSON(&new_note)
 
 	user := users.GetUserByid(new_note.AuthorId)
 	err := InsertToDb(Note{Text: new_note.Text, User: user, UserUsername: new_note.AuthorId})
-	if (err.Error != nil) {
-		c.JSON(http.StatusBadRequest, gin.H {
-			"Message": "Something went wrong",
+	if err.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"Message":       "Something went wrong",
 			"ErrorMesssage": err.Error,
 		})
 		return
 	}
-	c.JSON(http.StatusCreated, gin.H {
+	c.JSON(http.StatusCreated, gin.H{
 		"Message": "Note created",
 	})
 }
 
 func GetNote(c *gin.Context) {
+	// TODO: get user_id from jwt
+	var user_id string = "1"
 	note_id := c.Param("note_id")
+	value, _ := cache.GetKey(user_id)
+	if user_notes := value.GetValue(); user_notes != "-1" {
+		if ! strings.Contains(user_notes, note_id) {
+			c.JSON(http.StatusForbidden, gin.H{
+				"Message": "Access denied",
+			})
+			return
+		}
+	}
 	value, err := cache.GetKey(note_id)
 	if err != nil {
 		c.JSON(http.StatusOK, gin.H{
@@ -47,9 +59,7 @@ func GetNote(c *gin.Context) {
 		go cache.SetKey(note_id, text)
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"Text": text,
-	})
+	c.JSON(http.StatusOK, gin.H{"Text": text, })
 }
 
 func UpdateNote(c *gin.Context) {
@@ -78,4 +88,12 @@ func DeleteNote(c *gin.Context) {
 			"message": "delete_note",
 		})
 	}
+}
+
+func updateUserNotes(user_id string, new_note_id string) {
+	notes, err := cache.GetKey(fmt.Sprintf("u%s", user_id))
+	if err != nil {
+		panic(err)
+	}
+	cache.SetKey(fmt.Sprintf("u%s", user_id), fmt.Sprintf("%s %s", notes, new_note_id))
 }
