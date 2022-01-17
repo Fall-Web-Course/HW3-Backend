ccc8353b7c7349b4f0667ed2e3efd10f198f0e82
mhbahmani <mhbahmani79@gmail.com>
2022-01-17 14:41:00 +0330

    add loading .env file to main

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	init_db "github.com/Fall-Web-Course/HW3/db/init"
 	router "github.com/Fall-Web-Course/HW3/router"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load(".env")
 	init_db.InitDb()
 	router.InitRouter();
 }
